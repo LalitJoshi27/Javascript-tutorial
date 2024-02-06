6ee510d4ee56de55e012fc25873908064263c094
LalitJoshi27 <106146115+LalitJoshi27@users.noreply.github.com>
2024-02-06 15:49:30 +0000

    config done

---
+++ Jsbasics/variable.js
@@ -0,0 +1,18 @@
+const Account = 101
+console.log(Account)
+let accountEmail="joshi@gmail.com"
+var accountPassword= "Lalit123"
+console.log(accountPassword)
+console.log(accountEmail)
+accountPassword="Joshi"
+console.log(accountPassword)
+let account
+console.log(account)
+/*
+If we will not define any variable in javascript it will not give any error but will be undefined
+*/
+
+/*
+Prefer not to use var to declare a variable in javascript because of block scope & functional scope
+Use let to declare a variable in Javascript
+*/
