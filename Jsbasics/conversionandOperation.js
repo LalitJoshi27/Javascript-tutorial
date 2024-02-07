92336bd381c05bf6857e9266f62ed43ba9b0bd64
LalitJoshi27 <106146115+LalitJoshi27@users.noreply.github.com>
2024-02-07 08:43:04 +0000

    Coversion done

---
+++ Jsbasics/conversionandOperation.js
@@ -0,0 +1,14 @@
+let name= "lalit"
+console.log(typeof (name))
+let age= 15
+console.log(typeof age)
+let valueInNumber= Number(name)
+console.log(typeof valueInNumber)// it will show type of valueInNumber is Number
+console.log(valueInNumber)// NaN is not a value(Number)
+// true=> 1&& False=> 0
+let loggedIn
+let booleanisLoggedIn= Boolean(loggedIn)
+console.log(booleanisLoggedIn)
+let stringval= String(age)
+console.log(stringval)
+console.log(typeof stringval)
