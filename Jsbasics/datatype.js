48e6bb8f80e5d4a559f5e9077cce3df6c1c660e5
LalitJoshi27 <106146115+LalitJoshi27@users.noreply.github.com>
2024-02-07 08:26:41 +0000

    config done

---
+++ Jsbasics/datatype.js
@@ -0,0 +1,20 @@
+"use strict";// treat all Js code as new Js version
+//alert("HELLO LALIT") We are using Node js so it will not run if we will do it in browser it will run
+// Js is dynamically typed language
+let name="Lalit" //String
+let age= 22 //number
+let isLoggedIn= false //boolean
+
+// number => 2 power 53
+//bigint= for bigger numbers
+//string => "" ''
+//boolean =>true/false
+//null=> Standalone value(Representaion of empty value)
+//undefined=> NO value assign
+//symbol=> unique
+
+//object=> used for key and value
+
+console.log(typeof(age))
+console.log(typeof null)// type of null is object
+console.log(typeof undefined)// type of null is undefined
