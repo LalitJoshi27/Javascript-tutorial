95f373d7c7571e91a7d0167190c2cef7d52fe899
LalitJoshi27 <106146115+LalitJoshi27@users.noreply.github.com>
2024-02-08 15:58:57 +0000

    Non Premitive data type

--- Jsbasics/datatype.js
+++ Jsbasics/datatype.js
@@ -18,3 +18,45 @@ let isLoggedIn= false //boolean
 console.log(typeof(age))
 console.log(typeof null)// type of null is object
 console.log(typeof undefined)// type of null is undefined
+//2 type of data types:
+// 1.Primitive Data types
+
+//7 types:  String , Number, Boolean, null , undefined, Symbol, BigInt (call by value)
+ // Call by value is providing copy not the acutal refernce of data
+//*****//
+ //Javascript is Dynamically typed laguage
+// No need of declaring type of variables.
+
+const id= Symbol("121")
+const anotherid= Symbol("121")
+console.log(id==anotherid)
+// It will result false because value of id and anotherid will not be same
+//2. Reference or Non Primitive Data Types
+
+//Array, Object, Function
+const bignum= 1011003098938587876567684n
+//How to areate array
+let arr= [1,2,3,4];
+//How to create object
+let obj={
+    "firstname":"Lalit",
+    "Lastname": "Joshi"
+}
+//How to create Function
+let fun=function hello(){
+    console.log( "Hello")
+}
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+let a;
+console.log(obj);
+console.log(fun);
+console.log(typeof 10) //number
+console.log(typeof obj)// object
+console.log(typeof arr)// object
+console.log(typeof bignum)//BigInt
+console.log(typeof fun)//Function
+console.log(typeof null)//object
+console.log(typeof a)//undefined
+console.log(typeof id)//Symbol
