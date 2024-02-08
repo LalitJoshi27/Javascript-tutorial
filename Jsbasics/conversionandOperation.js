8a862b9c7a8052a8762cce5f154494b225d5a8df
LalitJoshi27 <106146115+LalitJoshi27@users.noreply.github.com>
2024-02-08 15:30:36 +0000

    strict check

--- Jsbasics/conversionandOperation.js
+++ Jsbasics/conversionandOperation.js
@@ -20,7 +20,20 @@ console.log(2+2)
 console.log(2*2)
 console.log(2**3)// this is used for power
 console.log(2%4)
+//+ is used for conversion in javascript
 console.log(typeof("2"+ 2))
 console.log(typeof(2+"2"))
 console.log("1"+2+2)
 console.log(typeof(2+2+"1"+1+1))
+console.log("2">1);
+console.log("02">1)
+//comparsion and equality check(condition) are different.
+console.log(null>0)
+console.log(null==0) //this will comparing only null to the zero so no conversion is there so it will returning false
+console.log(null>=0)// this will return true because null is coverted into a number ,null treated as 0 so 0>=0 it returns true
+console.log(null<=0)
+
+console.log(undefined==0)
+//Strict check ===
+console.log("2"==2)// this will covert 
+console.log("2"===2)// this will check data as well as datatypes
