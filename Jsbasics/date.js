4552064cc1cdb523b759917f18e9c9cbfbd52b47
LalitJoshi27 <106146115+LalitJoshi27@users.noreply.github.com>
2024-02-18 15:06:22 +0000

    date done

---
+++ Jsbasics/date.js
@@ -0,0 +1,11 @@
+let mydate= new Date();
+console.log(mydate);
+console.log(mydate.toDateString()); //This will print Sun Feb 18 2024
+console.log(mydate.toISOString()) //
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleTimeString()); // This will print the 
+console.log(mydate.toLocaleDateString());
+console.log(mydate.getTime());
+
+let createTime= new Date(24,1,18,8,34)
+console.log(createTime)
