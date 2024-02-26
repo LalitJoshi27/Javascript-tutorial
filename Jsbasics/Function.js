9405bee35dea6155086e66e50c4442b822d20a8a
LalitJoshi27 <106146115+LalitJoshi27@users.noreply.github.com>
2024-02-26 15:43:31 +0000

    Function is done

--- Jsbasics/Function.js
+++ Jsbasics/Function.js
@@ -25,3 +25,28 @@ console.log(LoggedIn("Lalit"))
 // then we can assign a default value to prameter in a function
 // for example function name(username="Lalit")
 // then if didn't pass anything in function calling then default value lalit is used for function
+// Here ...num1 ia used as rest method
+function CalculateCart(...num1){
+    return num1
+}
+//if we use fuction CalculateCart(val1,val2,...num){
+//    return num
+//} Here output will be 18 becaue 15 is val1 and 12 is val2 so rest of value in arguments is under ...num
+//console.log(CalculateCart(15,12,18)) // This will return the first value which is 15
+console.log(CalculateCart(15,12,18)) //if we want all of the value in an array we have to use rest function 
+
+// Passing Object in function
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.Username} Price is ${anyobject.Price}`)
+}
+handleObject({
+    Username:"Lalit",// here we can pass an object which can created earlier
+    Price:50000
+})
+
+// Passing Array in function
+const myArray= [10,20,30,40]
+function getSecondValue(getArry){
+    return getArry[1]
+}
+console.log(getSecondValue(myArray))
